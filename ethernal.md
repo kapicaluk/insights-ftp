058924a3a765cd779fdb0ffe103eb9c70086eb8b
Lukasz Kapica <kapicaluk@gmail.com>
2018-08-24 11:24:50 -0400

    working, styled cmd line

---
+++ ethernal.md
@@ -0,0 +1,32 @@
+xva6r2pfjd56nyyf@ethereal.email
+H3hJU5p2jWMzWpb9cx
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'xva6r2pfjd56nyyf@ethereal.email',
+        pass: 'H3hJU5p2jWMzWpb9cx'
+    }
+});
+
+SMTP configuration
+Host	smtp.ethereal.email
+Port	587
+Security	STARTTLS
+Username	xva6r2pfjd56nyyf@ethereal.email
+Password	H3hJU5p2jWMzWpb9cx
+
+IMAP configuration
+Host	imap.ethereal.email
+Port	993
+Security	TLS
+Username	xva6r2pfjd56nyyf@ethereal.email
+Password	H3hJU5p2jWMzWpb9cx
+
+POP3 configuration
+Host	pop3.ethereal.email
+Port	995
+Security	TLS
+Username	xva6r2pfjd56nyyf@ethereal.email
+Password	H3hJU5p2jWMzWpb9cx
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const dotenv = require('dotenv').config()
 const ftp = require('ftp')
 const zillowFtp = new ftp()
 const chalk = require('chalk')
-
 const today = new Date()
 const yesterdayUnformatted = new Date().setDate(today.getDate() - 1);
 const yesterday = new Date(yesterdayUnformatted)
-// setInterval(connectToZillowFtp,5000);  // 86400000 run this code every 24h 
+
+const fileNameFormatted = chalk.bold.green
+const errorMsgFormatted = chalk.bold.red
+const fileSizeFormatted = chalk.bold.magenta
+
+  // 86400000 run this code every 24h 
+// setInterval(connectToZillowFtp, 5000);
+
 //express.js 
 
 function connectToZillowFtp() {
@@ -14,12 +20,12 @@ function connectToZillowFtp() {
         zillowFtp.list(function (err, list) {
             if (err) console.log(err);
             let zillowSpreadsheets = list.filter(spreadsheet => { return spreadsheet.date.toDateString() === yesterday.toDateString() })
-            if (zillowSpreadsheets.length < 2) { console.log(chalk.bold.red('You\'re missing the most recent spreadsheet! Check Zillow FTP')) }
+            if (zillowSpreadsheets.length < 2) { console.log(errorMsgFormatted('You\'re missing the most recent spreadsheet! Check Zillow FTP')) }
             else {
                 console.log(
-                    chalk.bold.green('your last two files: ', '\n', 
-                     zillowSpreadsheets[0].name, chalk.bold.magenta('size:', Math.round((zillowSpreadsheets[0].size) / 1024), 'kb') ,'\n', '\n',
-                     zillowSpreadsheets[1].name, chalk.bold.magenta('size:', Math.round((zillowSpreadsheets[1].size) / 1024), 'kb')))
+                    fileNameFormatted('your last two files: ', '\n','\n', 
+                     zillowSpreadsheets[0].name, fileSizeFormatted('size:', Math.round((zillowSpreadsheets[0].size) / 1024), 'kb') ,'\n', '\n',
+                     zillowSpreadsheets[1].name, fileSizeFormatted('size:', Math.round((zillowSpreadsheets[1].size) / 1024), 'kb')))
             }
             zillowFtp.end()
         });
@@ -34,3 +40,4 @@ function connectToZillowFtp() {
 }
 
 connectToZillowFtp();
+
